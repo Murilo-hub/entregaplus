17d32ae08362e8109fa4708b268b0046b972de49
MuriloGB <105112352+Murilo-hub@users.noreply.github.com>
2025-05-26 11:16:53 -0300

    Delete entregaplus directory

--- entregaplus/app/utils.py
+++
@@ -1,7 +0,0 @@
-def get_user_ip():
-    from flask import request
-    if request.environ.get('HTTP_X_FORWARDED_FOR'):
-        ip = request.environ['HTTP_X_FORWARDED_FOR']
-    else:
-        ip = request.remote_addr
-    return ip
