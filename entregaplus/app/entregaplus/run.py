94e5cb6134faa181c32d47cb9b7af80cc25d8355
MuriloGB <105112352+Murilo-hub@users.noreply.github.com>
2025-05-26 20:57:03 -0300

    Delete entregaplus directory

--- entregaplus/app/entregaplus/run.py
+++
@@ -1,6 +0,0 @@
-from app import create_app #Importa a função de criação da app Flask.
-
-app = create_app() # Cria a aplicação usando a configuração padrão.
-
-if __name__ == '__main__': # Define o ponto de entrada do script.
-    app.run(debug=True) # Inicia o servidor com modo de depuração ativo.
